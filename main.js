0198dd2edc0341b96b4bc91fea2cbe36ae75d8d1
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2025-04-12 19:27:01 -0300

    Revert "oxi"
    This reverts commit 2c94233748c8ca9f8ed35fa00d96bf6afb7b01d1.

--- main.js
+++ main.js
@@ -62,10 +62,8 @@ function detectOS() {
       return 'macOS';
     case 'linux':
       return 'Linux';
-    case 'android':
-      return 'Lollipop'
     default:
-      return 'Sistema Operacional Desconhecido, por favor, tente entrar em um sistema que esteja disponível o programa.';
+      return 'Sistema Operacional Desconhecido';
   }
 }
 
